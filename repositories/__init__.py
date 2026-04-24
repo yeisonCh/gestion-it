7e7d7cb75e397ac25e22ad68eeb73b2f01cf00d7
DESKTOP-KLUH0HK\Yeison <yeisonchaparrohotmail.com>
2026-04-24 08:43:31 -0500

    Implementación crud con validación para empresa

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,6 +1,5 @@
 # app/repositories/__init__.py
 
-from .empresa import obtener_empresas, obtener_empresa_por_id, crear_empresa
 from .persona import obtener_personas, obtener_persona_por_id, crear_persona
 from .tipo_activo import (
     obtener_tipos_activo,
@@ -9,4 +8,13 @@ from .tipo_activo import (
     crear_tipo_activo,
     actualizar_tipo_activo,
     eliminar_tipo_activo
+)
+from .empresa import (
+    obtener_empresas,
+    obtener_empresa_por_id,
+    obtener_empresa_por_nit,
+    obtener_empresa_por_nombre,
+    crear_empresa,
+    actualizar_empresa,
+    eliminar_empresa
 )
--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,5 @@
-from .empresa import service_obtener_empresas, service_obtener_empresa_por_id, service_crear_empresa
+# app/services/__init__.py
+
 from .persona import service_obtener_personas, service_obtener_persona_por_id, service_crear_persona
 from .tipo_activo import (
     service_obtener_tipos_activo,
@@ -6,4 +7,11 @@ from .tipo_activo import (
     service_crear_tipo_activo,
     service_actualizar_tipo_activo,
     service_eliminar_tipo_activo
+)
+from .empresa import (
+    service_obtener_empresas,
+    service_obtener_empresa_por_id,
+    service_crear_empresa,
+    service_actualizar_empresa,
+    service_eliminar_empresa
 )
